f351482981e53e44e45807bb63af3823226157de
Gtt-rgb <1321895075@qq.com>
2020-04-02 01:19:04 +0800

    删除部分无效代码

--- 1_simple_regression.py
+++ 1_simple_regression.py
@@ -12,7 +12,8 @@ y_data = np.square(x_data) - 0.5 + noise
 
 # nn layer
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    with tf.name_scope('weights'):
+        Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
     biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases
     if activation_function is None:
@@ -24,6 +25,7 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
+a=tf.constant(1)
 
 # add hidden layer
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
@@ -40,22 +42,6 @@ sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-#plot image(exist problem)
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.scatter(x_data, y_data)
-plt.ion()
-
-
 #train
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
-    if i % 50 == 0:
-        try:
-            ax.lines.remove(lines[0])
-        except Exception:
-            pass
-        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
-        # plot the prediction
-        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
-        plt.pause(1)
