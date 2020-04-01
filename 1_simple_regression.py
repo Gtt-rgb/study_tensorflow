77487ce07ed21bb79bfaff0acf3200a4ec200868
Gtt-rgb <1321895075@qq.com>
2020-04-02 00:14:15 +0800

    简单回归问题

---
+++ 1_simple_regression.py
@@ -0,0 +1,61 @@
+#tensorflow 2.0
+#python 3.7.4
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+import numpy as np
+import matplotlib.pyplot as plt
+
+#data and function
+x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
+y_data = np.square(x_data) - 0.5 + noise
+
+# nn layer
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+#Compile model(loss and  optimizer)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+# init
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+#plot image(exist problem)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x_data, y_data)
+plt.ion()
+
+
+#train
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        # plot the prediction
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(1)
